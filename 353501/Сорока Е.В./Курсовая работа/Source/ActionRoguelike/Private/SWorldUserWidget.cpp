dcb68bfde1f1403790ec9ff72e893501ac451a25
Soroka E <e268006@gmail.com>
2024-06-04 23:35:16 +0300

    solved kursach

---
+++ 353501/Сорока Е.В./Курсовая работа/Source/ActionRoguelike/Private/SWorldUserWidget.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SWorldUserWidget.h"
+
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/SizeBox.h"
+#include "Kismet/GameplayStatics.h"
+
+void USWorldUserWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	if (!IsValid(AttachedActor))
+	{
+		RemoveFromParent();
+
+		UE_LOG(LogTemp, Warning, TEXT("AttachedActor no longer valid, removing Health Widget."))
+		return;
+	}
+	
+	FVector2d ScreenPosition;
+	bool bIsOnScreen = UGameplayStatics::ProjectWorldToScreen(GetOwningPlayer(), AttachedActor->GetActorLocation() + WorldOffset, ScreenPosition); 
+	
+	if (bIsOnScreen)
+	{
+		float Scale = UWidgetLayoutLibrary::GetViewportScale(this);
+
+		ScreenPosition /= Scale;
+
+		if (ParentSizeBox)
+		{
+			ParentSizeBox->SetRenderTranslation(ScreenPosition);
+		}
+	}
+
+	if (ParentSizeBox)
+	{
+		ParentSizeBox->SetVisibility(bIsOnScreen ? ESlateVisibility::HitTestInvisible : ESlateVisibility::Collapsed);
+	}
+}
