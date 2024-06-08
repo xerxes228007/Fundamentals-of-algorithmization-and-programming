13dbe042aa90815e01b2c45f127e6eb416b64665
LastJayG <lastjaygames@gmail.com>
2024-06-08 14:00:10 +0300

    lab6

---
+++ 353501/Малиновская Е.А./LabWork_6/Task_1/MyStruct.h
@@ -0,0 +1,14 @@
+#ifndef MYSTRUCT_H
+#define MYSTRUCT_H
+
+#endif // MYSTRUCT_H
+
+struct firm
+{
+    bool IsAbroad;
+    char Rating;
+    int WorkersQuantity;
+    double AverageSalary;
+    char NameOfFirm[15];
+    int Price[5];
+};
