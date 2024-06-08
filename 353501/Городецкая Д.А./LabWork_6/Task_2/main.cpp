b82a5bcd7fbf8c75e49c8c741bf589c2bc9cccfa
Dziyana Haradzetskaya <diana21032006w@gmail.com>
2024-06-08 12:47:45 +0300

    Add labs 3 5 6 7

---
+++ 353501/Городецкая Д.А./LabWork_6/Task_2/main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setStyleSheet("background-color: #000000;");
+    // a.setStyleSheet("QWidget { color: #000000; }");
+    w.show();
+    return a.exec();
+}
---
+++ 353501/Городецкая Д.А./LabWork_6/Task_2/plane.cpp
@@ -0,0 +1,4 @@
+#include "plane.h"
+
+char PLANE::name[10] = "";
+int PLANE::people_amount[10] = {0};
